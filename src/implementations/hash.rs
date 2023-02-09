cbdf5d392deaefee1d750c107ff00b927ef5400a
WinterCore <hogobbl@gmail.com>
2023-02-09 02:03:38 +0200

    feat: Implement basic hash file database

--- src/implementations/hash.rs
+++ src/implementations/hash.rs
@@ -1,14 +1,18 @@
 use std::collections::HashMap;
+use std::collections::VecDeque;
 
 use async_trait::async_trait;
 use std::path::Path;
 use tokio::fs;
 use tokio::io;
+use tokio::io::AsyncBufReadExt;
+use tokio::io::AsyncSeekExt;
+use tokio::io::AsyncWriteExt;
 
 use crate::implementations::ifc::Database;
 
 pub struct Hash {
-    segments: Vec<fs::File>,
+    segments: VecDeque<fs::File>,
     hash: HashMap<String, u64>,
     page_size: usize,
 }
@@ -16,7 +20,7 @@ pub struct Hash {
 impl Hash {
     pub fn new(page_size: usize) -> Hash {
         Hash {
-            segments: Vec::new(),
+            segments: VecDeque::new(),
             hash: HashMap::new(),
             page_size,
         }
@@ -29,6 +33,95 @@ impl Database for Hash {
         let path = Path::new("./.hash");
         fs::create_dir_all(path).await?;
 
+        let mut folders_stream = fs::read_dir(path).await?;
+        let mut all_files: Vec<String> = vec![];
+
+        while let Some(item) = folders_stream.next_entry().await? {
+            all_files.push(item.file_name().to_string_lossy().to_string());
+        }
+
+        let mut segment_files: Vec<String> = all_files
+            .into_iter()
+            .filter(|f| f.starts_with("segment-"))
+            .collect();
+
+        segment_files.sort();
+
+        // If we have no existing segment files, create one.
+        if segment_files.len() == 0 {
+            segment_files.push(format!("segment-{:0>6}", 0));
+        }
+
+        // TODO Improvement: Open these files in parallel
+        for file in segment_files.iter() {
+            let fd = fs::OpenOptions::new()
+                .read(true)
+                .append(true)
+                .create(true)
+                .open(path.join(file))
+                .await?;
+
+            self.segments.push_front(fd);
+        }
+
+        /*
+        let highest_segment: u32 = segment_files
+            .last()
+            .unwrap_or(&String::from("0"))
+            .as_str()
+            .parse()
+            .unwrap();
+        */
+
+        
         Ok(())
+    }
+
+    async fn reset(&mut self) -> io::Result<()> {
+        
+        Ok(())
+    }
+
+    async fn write(&mut self, key: &str, value: &str) -> io::Result<()> {
+        let file = self.segments.front_mut().expect("Uninitialized database");
+
+        file.write(format!("{}\u{038D}{}\n", key, value).as_bytes()).await?;
+
+        Ok(())
+    }
+
+    async fn read(&mut self, key: &str) -> io::Result<Option<String>> {
+        for file in self.segments.iter_mut() {
+            file.rewind().await?;
+
+            let mut reader = io::BufReader::new(file);
+
+            let mut found: Option<String> = None;
+
+            // Read the file line by line
+            loop {
+                let mut buffer = String::new();
+                let bytes = reader.read_line(&mut buffer).await?;
+
+                // Reached EOF
+                if bytes == 0 {
+                    break;
+                }
+
+                let curr_key: String = buffer.chars().take_while(|c| c != &'\u{038D}').collect();
+
+                if curr_key == key {
+                    let value: String = buffer.chars().skip(curr_key.len() + 1).collect();
+
+                    found = Some(value);
+                }
+            }
+
+            if let Some(value) = found {
+                return Ok(Some(value));
+            }
+        }
+
+        Ok(None)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,20 @@ mod implementations;
 use tokio::io;
 use implementations::basic::Basic;
 use implementations::ifc::Database;
+use implementations::hash::Hash;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut basic_db = Basic::new();
+    let mut db = Hash::new(1024 * 1024);
 
-    basic_db.init().await?;
-    basic_db.reset().await?;
-    /*
-    basic.write(&"1", &"onev2").await?;
-    basic.write(&"2", &"twov2").await?;
-    */
-    basic_db.write(&"5", &"hasan").await?;
-    let value = basic_db.read(&"5").await?;
+    db.init().await?;
+    db.reset().await?;
+    db.write(&"1", &"onev2").await?;
+    db.write(&"2", &"twov2").await?;
+    for i in 0..=10000 {
+        db.write(&"5", &format!("{}f ahsdklghasdlkghkl", i)).await?;
+    }
+    let value = db.read(&"5").await?;
 
     match value {
         Some(v) => println!("Found {}", v),
